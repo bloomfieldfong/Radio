2266510b94b80c62716fe1b8d9cd135b83b9a426
bloomfieldfong <bloomfieldf24@gmail.com>
2017-01-18 19:02:04 -0600

    pruebas finales

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,16 +1,16 @@
 import junit.framework.TestCase;
-import sun.tools.jar.resources.jar_it;
+
 
 public class Prueba extends TestCase {
 	
 	public void prueba(){
 		
 		Radio x = new Radio();
-		
 		float resultado = x.Seleccionar(1);
 		assertEquals(88.2,resultado);
 		
 		
 	}
+	
 
 }
