f935af97465b0b976dca5d19385ee74c6a383144
bloomfieldfong <bloomfieldf24@gmail.com>
2017-01-19 21:18:54 -0600

    Arreglos

--- src/IRadio.java
+++ src/IRadio.java
@@ -5,12 +5,26 @@
  *
  */
 
+
+
 public interface IRadio {
 
 	public void estado();
 	public void frecuencia();
+	/**
+	 * @param pos
+	 */
 	public void guardar(int pos);
+	/**
+	 * @param pos
+	 * @return nos devuelve la posiciones en la que esta 
+	 */
 	public float seleccionar(int pos);
+	
+	/**
+	 * 
+	 * @param cambio
+	 */
 	public void Cambiar(boolean cambio);
 	public boolean getFrecuencias();
 	public boolean getEstado();
